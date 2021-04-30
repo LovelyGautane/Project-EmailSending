43b05391a809ab024e01de58f25d25844794fffd
LovelyGautane <gautanelovely96@gmail.com>
2021-05-01 02:17:18 +0800

    Validating Fields Part

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Variables
     const sendBtn = document.getElementById('sendBtn'),
-        email = document.getElementById('email'),
-        subject = document.getElementById('subject'),
-        message = document.getElementById('message');
+            email = document.getElementById('email'),
+            subject = document.getElementById('subject'),
+            message = document.getElementById('message'),
+            reset = document.getElementById('resetbtn');	
+
 
 
 // Event Listeners
@@ -24,16 +26,47 @@ eventListeners();
 // Functions
 
     // App intitialization
-        function appInit() {
-            // disable the send button on load
-            sendBtn.disabled = true;
-        }
+    function appInit() {
+        // disable the send button on load
+        sendBtn.disabled = true;
+    }
 
     // Validate the form fields
     function validateField() {
-        let errors;
+      let errors;
 
-        // Validate the length of the field
-        validateLength(this);
+
+    // Validate the length of the field
+     validateLength(this);
+
+    // Validate email
+        if(this.type === 'email') {
+              validateEmail(this);
+        }
+    }
+
+    // Validate the length of the fields
+    function validateLength(field) {
+        if(field.value.length > 0 ) {
+            field.style.borderBottomColor = 'green';
+            field.classList.remove('error');
+        } else {
+            field.style.borderBottomColor = 'red';
+            field.classList.add('error');
+        }
+    }
+
+    // Validate email (checks for @ in the value)
+        function validateEmail(field) {
+        let emailText = field.value;
+
+        // check if the emailText contains the @ sign
+        if(emailText.indexOf('@') !== -1) {
+            field.style.borderBottomColor = 'green';
+            field.classList.remove('error');
+        } else {
+            field.style.borderBottomColor = 'red';
+            field.classList.add('error');
+        }
     }
 
